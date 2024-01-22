bd6d8ccfbbca86f0a9746f5b593a7fc017137540
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2024-01-23 01:09:32 +0300

    added interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'interceptor';
+  constructor(private _http: HttpClient) {}
+
+  getApi() {
+    /*
+    this._http
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .subscribe((res) => {
+        console.log(res);
+      });
+    */
+    /*
+    this._http.get('https://jsonplaceholder.typicode.com/todos').subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+      },
+    });
+    */
+    this._http
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TestInterceptor } from './test.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TestInterceptor,
+      multi: true,
+    },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
